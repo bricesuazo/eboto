2576c9859d377817a2c6f3c39bdd7fdd9f491e7e
Brice Suazo <bricesuazo@gmail.com>
2022-09-17 18:31:18 +0800

    Temporarily removed contents of _middleware.ts

--- _middleware.ts
+++ _middleware.ts
@@ -3,14 +3,14 @@ import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname.startsWith("/")) {
-    const session = getToken({
-      req,
-      secret: process.env.JWT_SECRET,
-      secureCookie: process.env.NODE_ENV === "production",
-    });
-    if (session !== null) {
-      return NextResponse.redirect("/dashboard");
-    }
-  }
+  // if (req.nextUrl.pathname.startsWith("/")) {
+  //   const session = getToken({
+  //     req,
+  //     secret: process.env.JWT_SECRET,
+  //     secureCookie: process.env.NODE_ENV === "production",
+  //   });
+  //   if (session !== null) {
+  //     return NextResponse.redirect("/dashboard");
+  //   }
+  // }
 }
