0f4b2b95055f9472e3c862e252c2881b537cf86c
Brice Suazo <bricesuazo@gmail.com>
2022-11-12 16:11:13 +0800

    Change all the dashboard path to :path* in middleware.

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,12 +10,7 @@ export const config = {
   matcher: [
     "/dashboard",
     "/:electionIdName/dashboard",
-    "/:electionIdName/dashboard/overview",
-    "/:electionIdName/dashboard/candidate",
-    "/:electionIdName/dashboard/partylist",
-    "/:electionIdName/dashboard/voter",
-    "/:electionIdName/dashboard/settings",
-    "/:electionIdName/dashboard/position",
+    "/:electionIdName/dashboard/:path*",
     "/create-election",
   ],
 };
