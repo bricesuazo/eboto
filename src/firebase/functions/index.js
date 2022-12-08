3adb78bf6f31ce65b0fe7aa4daa9ba3148390bed
Brice Suazo <bricesuazo@gmail.com>
2022-12-08 22:44:20 +0800

    Add email template to voters when election started.

--- src/firebase/functions/index.js
+++ src/firebase/functions/index.js
@@ -47,7 +47,67 @@ export const sendEmailToVoters = functions.pubsub
                 to: email,
                 message: {
                   subject: `Election Started ${election.data().name}`,
-                  html: `Hi ${fullName},<br/>Email: ${email}<br/>Initial password: ${password}`,
+                  html: `
+                  <body>
+                    <main
+                      style="
+                        max-width: 42rem;
+                        margin: auto;
+                        border: 1px solid #dedede;
+                        border-radius: 0.5rem;
+                        overflow: hidden;
+                        height: fit-content;
+                      "
+                    >
+                      <div style="padding: 1.5rem 3rem; background-color: #1b651b">
+                        <h2 style="color: white">${
+                          election.data().name
+                        } has started!</h2>
+                      </div>
+                      <div style="padding: 1.5rem 3rem">
+                        <p>Hello, <span style="font-weight: bold">${fullName}!</span></p>
+                        <p>
+                          An admin created an account for you as a voter in
+                          <a href="https://eboto-mo.com" target="_blank" style="color: #1b651b"
+                            >eBoto Mo.</a
+                          >
+                        </p>
+                        <blockquote style="line-height: 1rem">
+                          <h3>Your credentials</h3>
+                          <div style="line-height: 0.5rem">
+                            <p>
+                              Email:
+                              <span style="font-weight: bold">${email}</span>
+                            </p>
+                            <p>
+                              Password: <span style="font-weight: bold">${password}</span>
+                            </p>
+                          </div>
+                        </blockquote>
+                        <p>Always remember to vote wisely!</p>
+                        <a
+                          href="https://eboto-mo.com/signin"
+                          target="_blank"
+                          style="
+                            margin-top: 2rem;
+                            margin-left: auto;
+                            margin-right: auto;
+                            display: block;
+                            width: fit-content;
+                            padding: 1rem 4rem;
+                            background-color: #1b651b;
+                            color: white;
+                            text-align: center;
+                            border-radius: 1rem;
+                            text-decoration: none;
+                            font-weight: bold;
+                          "
+                          >Sign in
+                        </a>
+                      </div>
+                    </main>
+                  </body>
+                  `,
                 },
               })
               .then(() => {
