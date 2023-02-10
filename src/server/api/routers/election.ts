1a6fb53d9be39df72b2d7c42081627e505f001bf
Brice Suazo <bricesuazo@gmail.com>
2023-02-10 22:32:32 +0800

    Add create election api route.

--- src/server/api/routers/election.ts
+++ src/server/api/routers/election.ts
@@ -3,19 +3,37 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 
 export const electionRouter = createTRPCRouter({
-  // create: protectedProcedure
-  //   .input(z.object({ text: z.string() }))
-  //   .query(({ input }) => {
-  //     return {
-  //       greeting: `Hello ${input.text}`,
-  //     };
-  //   }),
+  create: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        slug: z.string(),
+        start_date: z.string(),
+        end_date: z.string(),
+        voting_start: z.number().nullish(),
+        voting_end: z.number().nullish(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const isElectionExists = await ctx.prisma.election.findUnique({
+        where: {
+          slug: input.slug,
+        },
+      });
 
-  // getAll: publicProcedure.query(({ ctx }) => {
-  //   return ctx.prisma.example.findMany();
-  // }),
+      if (isElectionExists) {
+        throw new Error("Election already exists");
+      }
 
-  // getSecretMessage: protectedProcedure.query(() => {
-  //   return "you can now see this secret message!";
-  // }),
+      return ctx.prisma.election.create({
+        data: {
+          name: input.name,
+          slug: input.slug,
+          start_date: input.start_date,
+          end_date: input.end_date,
+          voting_start: input.voting_start ? input.voting_start : undefined,
+          voting_end: input.voting_end ? input.voting_end : undefined,
+        },
+      });
+    }),
 });
