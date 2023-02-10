f1d9b24b9ee2d86051196583d7be62a66ac46026
Brice Suazo <bricesuazo@gmail.com>
2023-02-10 20:24:25 +0800

    Remove tailwindcss. Add Chakra UI.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { ChakraProvider } from '@chakra-ui/react'
 
 import { api } from "../utils/api";
 
-import "../styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +12,10 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <ChakraProvider>
+
       <Component {...pageProps} />
+      </ChakraProvider>
     </SessionProvider>
   );
 };
---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { ColorModeScript } from '@chakra-ui/react'
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang='en'>
+      <Head />
+      <body>
+        <ColorModeScript initialColorMode="system" />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
