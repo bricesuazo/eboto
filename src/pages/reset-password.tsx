972846e8b17144c42070466fa8357d8f42c45bad
Brice Suazo <bricesuazo@gmail.com>
2023-03-07 18:07:20 +0800

    Relayout signup and forgot password page.

--- src/pages/reset-password.tsx
+++ src/pages/reset-password.tsx
@@ -4,14 +4,24 @@ import {
   Button,
   Center,
   Container,
+  Group,
+  Paper,
+  rem,
   Stack,
+  Text,
   TextInput,
+  Title,
 } from "@mantine/core";
 import { useRouter } from "next/router";
 import { api } from "../utils/api";
 import { useEffect } from "react";
 import { isEmail, isNotEmpty, useForm } from "@mantine/form";
-import { IconAlertCircle, IconAt, IconCircleCheck } from "@tabler/icons-react";
+import {
+  IconAlertCircle,
+  IconArrowLeft,
+  IconAt,
+  IconCircleCheck,
+} from "@tabler/icons-react";
 import Link from "next/link";
 
 const ResetPassword = () => {
@@ -37,54 +47,99 @@ const ResetPassword = () => {
     api.user.requestResetPassword.useMutation();
 
   return (
-    <Container size="xs">
+    <Container size={420} my={40}>
       {requestResetPasswordMutation.isSuccess ? (
         <Alert icon={<IconCircleCheck />} title="Success!" color="green">
           Please check your email to reset your password.
         </Alert>
       ) : (
-        <form
-          onSubmit={form.onSubmit((data) => {
-            void (async () => {
-              await requestResetPasswordMutation.mutateAsync(data.email);
-              form.reset();
-            })();
-          })}
-        >
-          <Stack spacing="sm">
-            <TextInput
-              placeholder="Enter your email"
-              type="email"
-              withAsterisk
-              label="Email"
-              required
-              icon={<IconAt size="1rem" />}
-              {...form.getInputProps("email")}
-            />
-
-            {requestResetPasswordMutation.isError && (
-              <Alert
-                icon={<IconAlertCircle size="1rem" />}
-                title="Reset password error"
-                color="red"
-              >
-                {requestResetPasswordMutation.error?.message}
-              </Alert>
-            )}
-
-            <Button
-              type="submit"
-              loading={requestResetPasswordMutation.isLoading}
+        <>
+          <Title align="center" order={2}>
+            Forgot your password?
+          </Title>
+          <Text color="dimmed" size="sm" align="center" mt={5}>
+            Enter your email to get a reset link
+          </Text>
+          <Paper
+            withBorder
+            shadow="md"
+            sx={(theme) => ({
+              padding: theme.spacing.sm,
+              [theme.fn.largerThan("xs")]: { padding: theme.spacing.xl },
+            })}
+            mt={30}
+            radius="md"
+          >
+            <form
+              onSubmit={form.onSubmit((data) => {
+                void (async () => {
+                  await requestResetPasswordMutation.mutateAsync(data.email);
+                  form.reset();
+                })();
+              })}
             >
-              Reset password
-            </Button>
-            <Center>
-              <Anchor component={Link} href="/signin" size="sm">
-                Remembered your password? Sign in.
-              </Anchor>
-            </Center>
-          </Stack>
-        </form>
+              <Stack spacing="sm">
+                <TextInput
+                  placeholder="Enter your email"
+                  type="email"
+                  withAsterisk
+                  label="Email"
+                  required
+                  icon={<IconAt size="1rem" />}
+                  {...form.getInputProps("email")}
+                />
+
+                {requestResetPasswordMutation.isError && (
+                  <Alert
+                    icon={<IconAlertCircle size="1rem" />}
+                    title="Reset password error"
+                    color="red"
+                  >
+                    {requestResetPasswordMutation.error?.message}
+                  </Alert>
+                )}
+
+                <Group
+                  position="apart"
+                  sx={(theme) => ({
+                    [theme.fn.smallerThan("xs")]: {
+                      flexDirection: "column-reverse",
+                    },
+                  })}
+                >
+                  <Anchor
+                    color="dimmed"
+                    size="sm"
+                    sx={(theme) => ({
+                      [theme.fn.smallerThan("xs")]: {
+                        width: "100%",
+                        textAlign: "center",
+                      },
+                    })}
+                    component={Link}
+                    href="/signin"
+                  >
+                    <Center inline>
+                      <IconArrowLeft size={rem(12)} stroke={1.5} />
+                      <Text size="xs" ml={5}>
+                        Back to the login page
+                      </Text>
+                    </Center>
+                  </Anchor>
+                  <Button
+                    sx={(theme) => ({
+                      [theme.fn.smallerThan("xs")]: {
+                        width: "100%",
+                      },
+                    })}
+                  >
+                    Reset password
+                  </Button>
+                </Group>
+              </Stack>
+            </form>
+          </Paper>
+        </>
       )}
     </Container>
   );
