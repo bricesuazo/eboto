33f897c048a14d8177c53f3d8adcc7ec1e5cf9b3
Brice Suazo <bricesuazo@gmail.com>
2023-03-19 18:01:36 +0800

    Fix voter page header.

--- src/pages/dashboard/[electionSlug]/voter.tsx
+++ src/pages/dashboard/[electionSlug]/voter.tsx
@@ -1,10 +1,19 @@
-import { Button, Text, Table, TextInput, Flex } from "@mantine/core";
+import {
+  Button,
+  Text,
+  Table,
+  TextInput,
+  Flex,
+  Box,
+  Stack,
+} from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { IconSearch } from "@tabler/icons-react";
+import { IconSearch, IconUpload, IconUserPlus } from "@tabler/icons-react";
 import { useRouter } from "next/router";
 import CreateVoterModal from "../../../components/modals/CreateVoter";
 import Voter from "../../../components/Voter";
 import { api } from "../../../utils/api";
+import Balancer from "react-wrap-balancer";
 
 const DashboardVoter = () => {
   const router = useRouter();
@@ -35,41 +44,85 @@ const DashboardVoter = () => {
         refetch={voters.refetch}
       />
 
-      <Flex columnGap="sm">
-        <Button onClick={open}>Add voter</Button>
-        <TextInput
-          placeholder="Search by any field"
-          mb="md"
-          icon={<IconSearch size="0.9rem" stroke={1.5} />}
-          // value={search}
-          // onChange={handleSearchChange}
-          sx={{
-            flex: 1,
-          }}
-        />
-      </Flex>
+      <Stack>
+        <Flex
+          sx={(theme) => ({
+            gap: theme.spacing.xs,
 
-      <Table striped highlightOnHover withBorder>
-        <thead>
-          <tr>
-            <th>Email</th>
-            <th>
-              <Text align="center">Status</Text>
-            </th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          {voters.data.voters.map((voter) => (
-            <Voter
-              key={voter.id}
-              electionId={voters.data.election.id}
-              voter={voter}
-              refetch={voters.refetch}
-            />
-          ))}
-        </tbody>
-      </Table>
+            [theme.fn.smallerThan("xs")]: {
+              gap: theme.spacing.sm,
+              flexDirection: "column",
+            },
+          })}
+        >
+          <Flex gap="xs">
+            <Button
+              leftIcon={<IconUserPlus size="1rem" />}
+              onClick={open}
+              sx={(theme) => ({
+                [theme.fn.smallerThan("xs")]: {
+                  width: "100%",
+                },
+              })}
+            >
+              Add voter
+            </Button>
+            <Button
+              leftIcon={<IconUpload size="1rem" />}
+              variant="light"
+              sx={(theme) => ({
+                [theme.fn.smallerThan("xs")]: {
+                  width: "100%",
+                },
+              })}
+            >
+              Import
+            </Button>
+          </Flex>
+          <TextInput
+            placeholder="Search by any field"
+            icon={<IconSearch size="1rem" />}
+            // value={search}
+            // onChange={handleSearchChange}
+            sx={{
+              flex: 1,
+            }}
+          />
+        </Flex>
+
+        {!voters.data.voters.length ? (
+          <Box>
+            <Text align="center">
+              <Balancer>
+                No voters found. Add one by clicking the button above.
+              </Balancer>
+            </Text>
+          </Box>
+        ) : (
+          <Table striped highlightOnHover withBorder>
+            <thead>
+              <tr>
+                <th>Email</th>
+                <th>
+                  <Text align="center">Status</Text>
+                </th>
+                <th />
+              </tr>
+            </thead>
+
+            <tbody>
+              {voters.data.voters.map((voter) => (
+                <Voter
+                  key={voter.id}
+                  electionId={voters.data.election.id}
+                  voter={voter}
+                  refetch={voters.refetch}
+                />
+              ))}
+            </tbody>
+          </Table>
+        )}
+      </Stack>
     </>
   );
 };
