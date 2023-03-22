d6f6a6cf9dc0c60fa0cda2dc08cbe725410d3805
Brice Suazo <bricesuazo@gmail.com>
2023-03-22 21:33:12 +0800

    Fix excel upload.

--- src/components/modals/UploadBulkVoter.tsx
+++ src/components/modals/UploadBulkVoter.tsx
@@ -1,8 +1,19 @@
-import { Group, Modal, rem, Text, useMantineTheme } from "@mantine/core";
+import {
+  Button,
+  Flex,
+  Group,
+  Modal,
+  rem,
+  Stack,
+  Text,
+  useMantineTheme,
+} from "@mantine/core";
 import { Dropzone, MS_EXCEL_MIME_TYPE } from "@mantine/dropzone";
 import { IconFileSpreadsheet, IconUpload, IconX } from "@tabler/icons-react";
 import readXlsxFile, { type Row } from "read-excel-file";
 import { useState } from "react";
+import { useDidUpdate } from "@mantine/hooks";
+import Balancer from "react-wrap-balancer";
 
 const UploadBulkVoter = ({
   isOpen,
@@ -21,84 +32,181 @@ const UploadBulkVoter = ({
       fileName: string;
       voters: Row[];
     }[]
-  >();
-  console.log("selectedFile", selectedFiles);
+  >([]);
+
+  useDidUpdate(() => {
+    if (isOpen) {
+      setSelectedFiles([]);
+    }
+  }, [isOpen]);
   return (
     <Modal
       onClose={onClose}
       opened={isOpen}
       title={<Text weight={600}>Upload bulk voters</Text>}
-      size="lg"
     >
-      <Dropzone
-        onDrop={(files) => {
-          // if (selectedFile?.find((f) => f.fileName === file.name)) {
-          //   return;
-          // }
+      <Stack spacing="sm">
+        {selectedFiles.length ? (
+          <Stack>
+            {selectedFiles.map((file) => (
+              <Stack key={file.fileName} spacing="sm">
+                <Flex justify="space-between">
+                  <Text weight={600}>{file.fileName}</Text>
+                  <Button
+                    onClick={() => {
+                      setSelectedFiles((prev) => {
+                        if (prev) {
+                          return prev.filter(
+                            (f) => f.fileName !== file.fileName
+                          );
+                        } else {
+                          return [];
+                        }
+                      });
+                    }}
+                    color="red"
+                    compact
+                  >
+                    Delete
+                  </Button>
+                </Flex>
+                <Stack spacing="xs">
+                  {file.voters.map((voter) => (
+                    <Flex key={voter[0]?.toString()} justify="space-between">
+                      <Text>{voter[0]?.toString()}</Text>
+                      <Button
+                        onClick={() => {
+                          setSelectedFiles((prev) => {
+                            return prev
+                              .map((f) => {
+                                if (f.fileName === file.fileName) {
+                                  return {
+                                    ...f,
 
-          Array.from(files).forEach((file) => {
-            void (async () =>
-              await readXlsxFile(file).then((rows) => {
-                if (rows.length < 1) {
-                  return;
-                }
+                                    voters: f.voters.filter(
+                                      (v) => v[0] !== voter[0]
+                                    ),
+                                  };
+                                } else {
+                                  return f;
+                                }
+                              })
+                              .filter((f) => f.voters.length > 0);
+                          });
+                        }}
+                        compact
+                        color="red"
+                      >
+                        Delete
+                      </Button>
+                    </Flex>
+                  ))}
+                </Stack>
+              </Stack>
+            ))}
+          </Stack>
+        ) : (
+          <Dropzone
+            onDrop={(files) => {
+              // if (selectedFile?.find((f) => f.fileName === file.name)) {
+              //   return;
+              // }
 
-                if (
-                  rows[0] &&
-                  (rows[0][0] !== "full_name" || rows[0][1] !== "email")
-                ) {
-                  return;
-                }
+              Array.from(files).forEach((file) => {
+                void (async () =>
+                  await readXlsxFile(file).then((rows) => {
+                    if (rows.length < 1) {
+                      return;
+                    }
 
-                if (selectedFiles?.find((f) => f.fileName === file.name)) {
-                  return;
-                }
+                    if (rows[0] && rows[0][0] !== "email") {
+                      return;
+                    }
 
-                setSelectedFiles((prev) => {
-                  if (prev) {
-                    return [
-                      ...prev,
-                      { fileName: file.name, voters: rows.slice(1) },
-                    ];
-                  } else {
-                    return [{ fileName: file.name, voters: rows.slice(1) }];
+                    if (selectedFiles?.find((f) => f.fileName === file.name)) {
+                      return;
+                    }
+
+                    setSelectedFiles((prev) => {
+                      if (prev) {
+                        return [
+                          ...prev,
+                          { fileName: file.name, voters: rows.slice(1) },
+                        ];
+                      } else {
+                        return [{ fileName: file.name, voters: rows.slice(1) }];
+                      }
+                    });
+                  }))();
+              });
+            }}
+            onReject={(files) => console.log("rejected files", files)}
+            accept={MS_EXCEL_MIME_TYPE}
+          >
+            <Flex
+              direction="column"
+              align="center"
+              justify="center"
+              gap="md"
+              style={{ minHeight: rem(220), pointerEvents: "none" }}
+            >
+              <Dropzone.Accept>
+                <IconUpload
+                  size="3.2rem"
+                  stroke={1.5}
+                  color={
+                    theme.colors.green[theme.colorScheme === "dark" ? 4 : 6]
                   }
-                });
-              }))();
-          });
-        }}
-        onReject={(files) => console.log("rejected files", files)}
-        accept={MS_EXCEL_MIME_TYPE}
-      >
-        <Group
-          position="center"
-          spacing="xl"
-          style={{ minHeight: rem(220), pointerEvents: "none" }}
-        >
-          <Dropzone.Accept>
-            <IconUpload
-              size="3.2rem"
-              stroke={1.5}
-              color={theme.colors.green[theme.colorScheme === "dark" ? 4 : 6]}
-            />
-          </Dropzone.Accept>
-          <Dropzone.Reject>
-            <IconX
-              size="3.2rem"
-              stroke={1.5}
-              color={theme.colors.red[theme.colorScheme === "dark" ? 4 : 6]}
-            />
-          </Dropzone.Reject>
-          <Dropzone.Idle>
-            <IconFileSpreadsheet size="3.2rem" stroke={1.5} />
-          </Dropzone.Idle>
-          <div>
-            <Text size="xl" align="center">
-              Drag excel file here or click to select files
-            </Text>
-          </div>
-        </Group>
-      </Dropzone>
+                />
+              </Dropzone.Accept>
+              <Dropzone.Reject>
+                <IconX
+                  size="3.2rem"
+                  stroke={1.5}
+                  color={theme.colors.red[theme.colorScheme === "dark" ? 4 : 6]}
+                />
+              </Dropzone.Reject>
+              <Dropzone.Idle>
+                <IconFileSpreadsheet size="3.2rem" stroke={1.5} />
+              </Dropzone.Idle>
+              <div>
+                <Text size="xl" align="center">
+                  <Balancer>
+                    Drag excel file here or click to select files
+                  </Balancer>
+                </Text>
+              </div>
+            </Flex>
+          </Dropzone>
+        )}
+        <Flex justify="space-between">
+          <Button
+            color="red"
+            onClick={() => {
+              setSelectedFiles([]);
+            }}
+            disabled={selectedFiles.length === 0}
+          >
+            Clear all
+          </Button>
+          <Group position="right" spacing="xs">
+            <Button
+              variant="default"
+              onClick={onClose}
+              //   disabled={createVoterMutation.isLoading}
+            >
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+              disabled={selectedFiles.length === 0}
+              //   loading={createVoterMutation.isLoading}
+            >
+              Upload
+            </Button>
+          </Group>
+        </Flex>
+      </Stack>
     </Modal>
   );
 };
