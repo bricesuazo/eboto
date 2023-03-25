5f8791f4ea5e8540bd3e4f8c7e842753c8c78adf
Brice Suazo <bricesuazo@gmail.com>
2023-03-25 16:49:35 +0800

    Fix layout in realtime page.

--- src/pages/[electionSlug]/realtime.tsx
+++ src/pages/[electionSlug]/realtime.tsx
@@ -1,16 +1,32 @@
-import { Container, Stack, Box, Text } from "@mantine/core";
+import {
+  Container,
+  Text,
+  Table,
+  Flex,
+  SimpleGrid,
+  Title,
+  Center,
+  Stack,
+  Box,
+  Group,
+} from "@mantine/core";
 import type { Election } from "@prisma/client";
+import { IconFingerprint } from "@tabler/icons-react";
 import type { GetServerSideProps, GetServerSidePropsContext } from "next";
+import Image from "next/image";
+import Moment from "react-moment";
 import { getServerAuthSession } from "../../server/auth";
 import { prisma } from "../../server/db";
 import { api } from "../../utils/api";
+import { convertNumberToHour } from "../../utils/convertNumberToHour";
+import Balancer from "react-wrap-balancer";
 
 const RealtimePage = ({ election }: { election: Election }) => {
   const positions = api.election.getElectionRealtime.useQuery(election.id, {
     refetchOnWindowFocus: false,
     refetchOnMount: false,
     refetchOnReconnect: false,
-    //   refetchInterval: 1000,
+    // refetchInterval: 1000,
   });
 
   if (positions.isLoading) return <div>Loading...</div>;
@@ -19,24 +35,91 @@ const RealtimePage = ({ election }: { election: Election }) => {
   if (!positions.data) return <div>No data</div>;
   return (
     <Container>
-      <Text>{election.name}</Text>
-
-      <h2>Positions</h2>
-      <Stack spacing={4}>
-        {positions.data.map((position) => (
-          <Box key={position.id}>
-            <Text>
-              {position.name} - {position.vote.length}
+      <Stack spacing="xl">
+        <Center>
+          <Box>
+            <Group position="center">
+              {election.logo ? (
+                <Image src={election.logo} alt="Logo" width={92} height={92} />
+              ) : (
+                <IconFingerprint size={92} style={{ padding: 8 }} />
+              )}
+            </Group>
+            <Title order={2} lineClamp={2} align="center">
+              {election.name} (@{election.slug})
+            </Title>
+            <Text align="center">
+              <Moment format="MMMM DD, YYYY" date={election.start_date} />
+              {" - "}
+              <Moment format="MMMM DD, YYYY" date={election.end_date} />
             </Text>
-            <Stack>
-              {position.candidate.map((candidate) => (
-                <Box key={candidate.id}>
-                  - {candidate.first_name} - {candidate.vote.length}
-                </Box>
-              ))}
-            </Stack>
+            <Text align="center">
+              Open from {convertNumberToHour(election.voting_start)} to{" "}
+              {convertNumberToHour(election.voting_end)}
+            </Text>
+            <Text align="center" size="xs" color="dimmed">
+              <Balancer>
+                Realtime result as of{" "}
+                <Moment date={new Date()} format="MMMM Do YYYY, h:mm:ss a" />
+              </Balancer>
+            </Text>
           </Box>
-        ))}
+        </Center>
+        <SimpleGrid
+          cols={3}
+          breakpoints={[
+            { maxWidth: "md", cols: 2, spacing: "md" },
+            { maxWidth: "xs", cols: 1, spacing: "sm" },
+          ]}
+        >
+          {positions.data.map((position) => (
+            <Table
+              key={position.id}
+              striped
+              highlightOnHover
+              withBorder
+              captionSide="bottom"
+              h="fit-content"
+            >
+              {/* <caption>
+              As of{" "}
+              <Moment date={new Date()} format="MMMM Do YYYY, h:mm:ss a" />
+            </caption> */}
+              <thead>
+                <tr>
+                  <th>
+                    <Text lineClamp={2}>{position.name}</Text>
+                  </th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {position.candidate.map((candidate) => (
+                  <tr key={candidate.id}>
+                    <td>
+                      <Flex justify="space-between" align="center">
+                        <Text lineClamp={2}>{`${candidate.last_name}, ${
+                          candidate.first_name
+                        }${candidate.middle_name ?? " "} (${
+                          candidate.partylist.acronym
+                        })`}</Text>
+                        <Text>{candidate.vote.length}</Text>
+                      </Flex>
+                    </td>
+                  </tr>
+                ))}
+                <tr>
+                  <td>
+                    <Flex justify="space-between">
+                      <Text>Abstain</Text>
+                      <Text>{position.vote.length}</Text>
+                    </Flex>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          ))}
+        </SimpleGrid>
       </Stack>
     </Container>
   );
