c286fcb5e7bff6c3aa493fc5adf7f20d39229a3a
Brice Suazo <bricesuazo@gmail.com>
2023-04-06 18:44:56 +0800

    Fix do-election-processing.

--- src/pages/api/do-election-processing.tsx
+++ src/pages/api/do-election-processing.tsx
@@ -3,19 +3,22 @@ import { verifySignature } from "@upstash/qstash/nextjs";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { sendEmailTransport } from "../../../emails";
 import ElectionInvitation from "../../../emails/ElectionInvitation";
+import { env } from "../../env.mjs";
 import { prisma } from "../../server/db";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const now = new Date(new Date(new Date().toLocaleString().slice(0, 8)));
+  now.setDate(now.getDate() - 1);
+  const start_date =
+    env.NODE_ENV === "production"
+      ? now.toISOString().split("T")[0]?.concat("T16:00:00.000Z")
+      : new Date(new Date(new Date().toLocaleString().slice(0, 8)));
+
   const elections = await prisma.election.findMany({
     where: {
-      start_date: new Date(new Date().toISOString().slice(0, 10)),
+      start_date,
     },
   });
-  console.log(
-    "🚀 ~ file: do-election-processing.tsx:14 ~ handler ~ elections:",
-    elections
-  );
-  console.log("date now", new Date(new Date().toISOString().slice(0, 10)));
 
   for (const election of elections) {
     await prisma.election.update({
@@ -34,30 +37,30 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     });
 
     for (const voter of invitedVoters) {
-      // const token = await prisma.verificationToken.create({
-      //   data: {
-      //     expiresAt: election.end_date,
-      //     type: "ELECTION_INVITATION",
-      //     invitedVoter: {
-      //       connect: {
-      //         id: voter.id,
-      //       },
-      //     },
-      //   },
-      // });
+      const token = await prisma.verificationToken.create({
+        data: {
+          expiresAt: election.end_date,
+          type: "ELECTION_INVITATION",
+          invitedVoter: {
+            connect: {
+              id: voter.id,
+            },
+          },
+        },
+      });
 
-      // await sendEmailTransport({
-      //   email: voter.email,
-      //   subject: `You have been invited to vote in ${election.name}`,
-      //   html: render(
-      //     <ElectionInvitation
-      //       type="VOTER"
-      //       token={token.id}
-      //       electionName={election.name}
-      //       electionEndDate={election.end_date}
-      //     />
-      //   ),
-      // });
+      await sendEmailTransport({
+        email: voter.email,
+        subject: `You have been invited to vote in ${election.name}`,
+        html: render(
+          <ElectionInvitation
+            type="VOTER"
+            token={token.id}
+            electionName={election.name}
+            electionEndDate={election.end_date}
+          />
+        ),
+      });
 
       await prisma.invitedVoter.update({
         where: {
@@ -73,11 +76,10 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).end();
 }
 
-export default handler;
-// export default verifySignature(handler);
+export default verifySignature(handler);
 
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
