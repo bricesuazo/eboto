fd2447b24d7721e818da93d2b106555ed2e3533f
Brice Suazo <bricesuazo@gmail.com>
2023-04-09 18:16:15 +0800

    Style GenerateResult

--- src/pdf/GenerateResult.tsx
+++ src/pdf/GenerateResult.tsx
@@ -3,148 +3,197 @@ import {
   Page,
   Text,
   View,
-  // StyleSheet,
-  // Image,
+  Image as PdfImage,
 } from "@react-pdf/renderer";
 
 import Moment from "react-moment";
-
-// Create styles
-// const styles = StyleSheet.create({
-//   page: {
-//     flexDirection: "row",
-//   },
-// });
+import { convertNumberToHour } from "../utils/convertNumberToHour";
 
 export interface ResultType {
-  result: {
+  id: string;
+  name: string;
+  slug: string;
+  logo: string | null;
+  start_date: Date;
+  end_date: Date;
+  voting_start: number;
+  voting_end: number;
+  positions: {
     id: string;
     name: string;
-    slug: string;
-    logo: string | null;
-    start_date: Date;
-    end_date: Date;
-    voting_start: number;
-    voting_end: number;
-    positions: {
+    votes: number;
+    candidates: {
       id: string;
       name: string;
       votes: number;
-      candidates: {
-        id: string;
-        name: string;
-        votes: number;
-      }[];
     }[];
-  };
+  }[];
 }
 
-// Create Document Component
-const GenerateResult = ({ result }: ResultType) => (
+const GenerateResult = ({ result }: { result: ResultType }) => (
   <Document>
     <Page
       size="A4"
       style={{
+        fontFamily: "Helvetica",
+        padding: 40,
         fontSize: 12,
+        paddingBottom: 80,
       }}
     >
-      <View style={{ margin: 32 }}>
+      <View
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: 20,
+        }}
+      >
+        <View style={{ display: "flex", alignItems: "center" }}>
+          <PdfImage
+            src="/images/eboto-mo-logo.png"
+            style={{ width: 32, height: 32 }}
+          />
+          <Text
+            style={{
+              fontWeight: 20,
+              fontFamily: "Helvetica-Bold",
+              color: "#2f9e44",
+            }}
+          >
+            eBoto Mo
+          </Text>
+        </View>
+      </View>
+      <View>
         <View
           style={{
             flexDirection: "column",
             textAlign: "center",
-            gap: 10,
             marginBottom: 20,
           }}
         >
+          {result.logo && (
+            <PdfImage
+              src={result.logo}
+              style={{
+                display: "flex",
+                width: 80,
+                height: 80,
+                alignSelf: "center",
+              }}
+            />
+          )}
           <Text
             style={{
-              fontSize: 20,
-              fontWeight: 500,
+              fontFamily: "Helvetica-Bold",
+              fontSize: 18,
             }}
           >
             {result.name}
           </Text>
           <Text>https://eboto-mo.com/{result.slug}</Text>
-
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "center",
-            }}
-          >
+          <Text>
             <Moment
-              format="MMMM DD, YYYY, hA"
+              format="MMMM DD, YYYY"
               element={Text}
               date={result.start_date}
             />
-            <Text> - </Text>
+            {" - "}
 
             <Moment
-              format="MMMM DD, YYYY, hA"
+              format="MMMM DD, YYYY"
               element={Text}
               date={result.end_date}
             />
+          </Text>
+          <Text>
+            Open from {convertNumberToHour(result.voting_start)} to{" "}
+            {convertNumberToHour(result.voting_end)}
+          </Text>
+          <View style={{ marginVertical: 8 }}>
+            <Text>Generated on:</Text>
+
+            <Moment
+              format="MMMM DD, YYYY, h:mmA"
+              element={Text}
+              date={new Date()}
+            />
           </View>
+          <Text style={{ fontFamily: "Helvetica-Bold" }}>Results</Text>
         </View>
 
-        <View>
-          <Text>Generated on:</Text>
-
-          <Moment format="MMMM DD, YYYY, h:mmA" element={Text}>
-            {new Date()}
-          </Moment>
-        </View>
-
-        <Text
+        <View
           style={{
-            fontSize: 16,
-            fontWeight: 500,
+            display: "flex",
+            flexDirection: "column",
+            gap: 20,
+            alignItems: "center",
           }}
         >
-          Results
-        </Text>
-      </View>
-
-      <View
-        style={{
-          fontSize: 12,
-        }}
-      >
-        {result.positions
-          // ?.sort((a, b) => a.order - b.order)
-          .map((position) => {
+          {result.positions.map((position) => {
             return (
-              <View key={position.id} style={{ marginBottom: 16 }}>
-                <Text
-                  style={{
-                    fontSize: 14,
-                    marginBottom: 4,
-                  }}
-                >
-                  {position.name}
-                </Text>
+              <View
+                key={position.id}
+                style={{
+                  width: 400,
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: 8,
+                }}
+              >
+                <View>
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      textAlign: "center",
+                      fontFamily: "Helvetica-Bold",
+                    }}
+                  >
+                    {position.name}
+                  </Text>
+                  <Text
+                    style={{
+                      textAlign: "center",
+                    }}
+                  >
+                    Abstain Count - {position.votes}
+                  </Text>
+                </View>
 
-                {position.candidates
-                  .sort((a, b) => b.votes - a.votes)
-                  .map((candidate) => {
-                    return (
-                      <View
-                        key={candidate.id}
-                        style={{
-                          marginBottom: 4,
-                        }}
-                      >
-                        <Text>{candidate.name}</Text>
-                        <Text>{candidate.votes} vote/s</Text>
-                      </View>
-                    );
-                  })}
-
-                <Text>Abstain - {position.votes} vote/s</Text>
+                <View>
+                  {position.candidates
+                    .sort((a, b) => b.votes - a.votes)
+                    .map((candidate, idx) => {
+                      return (
+                        <View
+                          key={candidate.id}
+                          style={{
+                            marginBottom: 4,
+                            backgroundColor:
+                              idx === 0 ? "#90ee90" : "transparent",
+                            padding: 8,
+                          }}
+                        >
+                          <Text>{candidate.name}</Text>
+                          <Text>
+                            <Text
+                              style={{
+                                fontFamily: "Helvetica-Bold",
+                              }}
+                            >
+                              {candidate.votes}
+                            </Text>{" "}
+                            vote{candidate.votes > 1 && "s"}
+                          </Text>
+                        </View>
+                      );
+                    })}
+                </View>
               </View>
             );
           })}
+        </View>
       </View>
     </Page>
   </Document>
