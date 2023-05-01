43e2b887240228340a2829d8b1e34a5e2e1ee34f
Brice Suazo <bricesuazo@gmail.com>
2023-05-01 19:36:06 +0800

    remove the invitation page in middleware. not require authentication.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/invitation", "/account/:path*"],
+  matcher: ["/dashboard/:path*", "/account/:path*"],
 };
