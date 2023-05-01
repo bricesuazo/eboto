fd4cf4aab8df7879143068b1aa5fbf61169049d9
Brice Suazo <bricesuazo@gmail.com>
2023-05-01 19:38:48 +0800

    Bring back invitation page in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/account/:path*"],
+  matcher: ["/dashboard/:path*", "/invitation", "/account/:path*"],
 };
