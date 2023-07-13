7cca438fb32d382a9ff27f4f7173ca8961f98927
Brice Suazo <bricesuazo@gmail.com>
2023-07-13 23:32:56 +0800

    Add metadata

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,11 +1,49 @@
 import "@mantine/core/styles.css";
 
 import { MantineProvider, ColorSchemeScript } from "@mantine/core";
+import { type Metadata } from "next";
+import { siteConfig } from "@/config/site";
 
-export const metadata = {
-  title: "eBoto Mo – Your One-Stop Online Voting Solution",
-  description:
-    "Empower your elections with eBoto Mo, the versatile and web-based voting platform that offers secure online elections for any type of organization.",
+export const metadata: Metadata = {
+  title: siteConfig.name,
+  description: siteConfig.description,
+  keywords: ["eBoto Mo", "Vote", "Election", "Voting System"],
+  authors: [
+    {
+      name: "Brice Suazo",
+      url: "https://bricesuazo.com",
+    },
+  ],
+  creator: "Brice Suazo",
+  openGraph: {
+    type: "website",
+    locale: "en_PH",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@brice_suazo",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
 };
 
 export default function RootLayout({
