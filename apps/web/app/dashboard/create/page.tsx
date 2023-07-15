99a02a7e5694cb00c5c037a1c7afe35a3879edfc
Brice Suazo <bricesuazo@gmail.com>
2023-07-15 10:18:30 +0800

    Fix with-env in root directory

---
+++ apps/web/app/dashboard/create/page.tsx
@@ -0,0 +1,11 @@
+import { db } from "@eboto-mo/db";
+import { example } from "@eboto-mo/db/schema";
+
+export default async function Page() {
+  const test = await db.insert(example).values({
+    id:
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15),
+  });
+  return <div>page</div>;
+}
--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,3 +1,8 @@
+import { db } from "@eboto-mo/db";
+import { example } from "@eboto-mo/db/schema";
+
 export default async function Page() {
+  const test = await db.select().from(example);
+  console.log("🚀 ~ file: page.tsx:6 ~ Page ~ test:", test);
   return <div></div>;
 }
