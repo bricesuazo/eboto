0293eed7eb005df9c9b6413f078eddc18b1a953a
Brice Suazo <bricesuazo@gmail.com>
2023-08-06 20:18:03 +0800

    Update to mantine@7.0.0-alpha.21

--- apps/www/app/(main)/about/page.tsx
+++ apps/www/app/(main)/about/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { api } from "@/trpc/client";
+// import { api } from "@/trpc/client";
 
 // import { api } from "@/lib/api/api";
 
 export default function page() {
   return (
     <div>
-      <button onClick={async () => await api.auth.test.mutate()}>
+      {/* <button onClick={async () => await api.auth.test.mutate()}>
         asdasdasd
-      </button>
+      </button> */}
       {/* <p>{api.auth.test.}</p> */}
     </div>
   );
