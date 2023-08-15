e5f6c4fe32e2193551913205b00526a9cdd5c188
Brice Suazo <bricesuazo@gmail.com>
2023-08-15 20:11:56 +0800

    Add initial page for election page.

--- apps/www/app/(main)/[electionSlug]/page.tsx
+++ apps/www/app/(main)/[electionSlug]/page.tsx
@@ -1,5 +1,292 @@
-import React from "react";
+import ScrollToTopButton from "@/components/client/components/scroll-to-top";
+import QRCode from "@/components/client/modals/show-qr-code";
+import { isElectionOngoing } from "@/utils";
+import { db } from "@eboto-mo/db";
+import {
+  Box,
+  Button,
+  Container,
+  Group,
+  Stack,
+  Text,
+  Title,
+  UnstyledButton,
+} from "@mantine/core";
+import { IconClock, IconFingerprint, IconUser } from "@tabler/icons-react";
+import moment from "moment";
+import type { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import Balancer from "react-wrap-balancer";
 
-export default function page() {
-  return <div>page</div>;
+export async function generateMetadata({
+  params: { electionSlug },
+}: {
+  params: { electionSlug: string };
+}): Promise<Metadata> {
+  const election = await db.query.elections.findFirst({
+    where: (election, { eq }) => eq(election.slug, electionSlug),
+  });
+
+  if (!election) return notFound();
+
+  return {
+    title: election.name,
+    description: `See details about ${election.name} | eBoto Mo`,
+    openGraph: {
+      title: election.name,
+      description: `See details about ${election.name} | eBoto Mo`,
+      images: [
+        {
+          url: `${
+            process.env.NODE_ENV === "production"
+              ? "https://eboto-mo.com"
+              : "http://localhost:3000"
+          }/api/og?type=election&election_name=${encodeURIComponent(
+            election.name,
+          )}&election_logo=${encodeURIComponent(
+            election.logo ?? "",
+          )}&election_date=${encodeURIComponent(
+            moment(election.start_date).format("MMMM D, YYYY hA") +
+              " - " +
+              moment(election.end_date).format("MMMM D, YYYY hA"),
+          )}`,
+          width: 1200,
+          height: 630,
+          alt: election.name,
+        },
+      ],
+    },
+  };
+}
+
+export default async function ElectionPage({
+  params: { electionSlug },
+}: {
+  params: { electionSlug: string };
+}) {
+  const election = await db.query.elections.findFirst({
+    where: (election, { eq }) => eq(election.slug, electionSlug),
+  });
+
+  if (!election) return notFound();
+
+  const isOngoing = isElectionOngoing({ election });
+
+  // const session = await currentUser();
+  // TODO: Add authorization here.
+  // https://github.com/bricesuazo/eboto-mo/blob/main/src/pages/%5BelectionSlug%5D/index.tsx
+
+  const positions = await db.query.positions.findMany({
+    where: (position, { eq }) => eq(position.election_id, election.id),
+    with: {
+      candidates: {
+        with: {
+          partylist: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <>
+      <ScrollToTopButton />
+
+      <Container py="xl">
+        {positions.length === 0 ? (
+          <Text>
+            This election has no positions. Please contact the election
+            commissioner for more information.
+          </Text>
+        ) : (
+          <>
+            <QRCode election={election} />
+            <Stack align="center">
+              <Box>
+                <Group align="center" mb={8}>
+                  {election.logo ? (
+                    <Image
+                      src={election.logo}
+                      alt="Logo"
+                      width={92}
+                      height={92}
+                      priority
+                    />
+                  ) : (
+                    <IconFingerprint size={92} style={{ padding: 8 }} />
+                  )}
+                </Group>
+
+                <Title order={2} ta="center" maw={600}>
+                  {election.name} (@{election.slug})
+                </Title>
+
+                <Text ta="center">
+                  {moment(election.start_date)
+                    .local()
+                    .format("MMMM DD, YYYY hA (dddd)")}
+                  {" - "}
+                  {moment(election.end_date)
+                    .local()
+                    .format("MMMM DD, YYYY hA (dddd)")}
+                </Text>
+                <Text ta="center">
+                  Publicity:{" "}
+                  {(() => {
+                    switch (election.publicity) {
+                      case "PRIVATE":
+                        return "Private (Only commissioners can see this election)";
+                      case "VOTER":
+                        return "Voter (Only voters and commissioners can see this election)";
+                      case "PUBLIC":
+                        return "Public (Everyone can see this election)";
+                      default:
+                        return null;
+                    }
+                  })()}
+                </Text>
+
+                {election.description && (
+                  <Box maw="40rem" mt="sm" ta="center">
+                    <Text>About this election:</Text>
+                    {/* <Spoiler
+                      maxHeight={50}
+                      showLabel="Show more"
+                      hideLabel="Hide"
+                    >
+                      {election.description}
+                    </Spoiler> */}
+                  </Box>
+                )}
+
+                <Group align="center" mt={8}>
+                  {
+                    // hasVoted ||
+                    election.end_date < new Date() ? (
+                      <Button
+                        radius="xl"
+                        size="md"
+                        component={Link}
+                        leftSection={<IconClock />}
+                        href={`/${election.slug}/realtime`}
+                      >
+                        Realtime count
+                      </Button>
+                    ) : !isOngoing ? (
+                      <Text c="red">Voting is not yet open</Text>
+                    ) : (
+                      <Button
+                        radius="xl"
+                        size="md"
+                        leftSection={<IconFingerprint />}
+                        component={Link}
+                        href={`/${election.slug}/vote`}
+                      >
+                        Vote now!
+                      </Button>
+                    )
+                  }
+                  <QRCode election={election} />
+                </Group>
+              </Box>
+
+              <Stack gap="lg">
+                {positions.map((position) => (
+                  <Stack gap={4} key={position.id}>
+                    <Title
+                      order={3}
+                      tw="bold"
+                      ta="center"
+                      style={{ lineClamp: 2 }}
+                    >
+                      <Balancer>{position.name}</Balancer>
+                    </Title>
+
+                    <Group align="center" gap="sm">
+                      {position.candidates.length === 0 ? (
+                        <Text>No candidates</Text>
+                      ) : (
+                        position.candidates.map((candidate) => (
+                          <UnstyledButton
+                            component={Link}
+                            href={`/${election?.slug || ""}/${candidate.slug}`}
+                            // style={(theme) => ({
+                            //   display: "flex",
+                            //   flexDirection: "column",
+                            //   alignItems: "center",
+                            //   width: 200,
+                            //   height: 192,
+                            //   padding: theme.spacing.xs,
+                            //   borderRadius: theme.radius.md,
+                            //   transition: "background-color 0.2s ease",
+                            //   // backgroundColor:
+                            //   //   theme.colorScheme === "dark"
+                            //   //     ? theme.colors.dark[6]
+                            //   //     : theme.colors.gray[0],
+                            //   // "&:hover": {
+                            //   //   backgroundColor:
+                            //   //     theme.colorScheme === "dark"
+                            //   //       ? theme.colors.dark[5]
+                            //   //       : theme.colors.gray[1],
+                            //   // },
+
+                            //   // [theme.fn.smallerThan("xs")]: {
+                            //   //   width: "100%",
+                            //   //   flexDirection: "row",
+                            //   //   justifyContent: "flex-start",
+                            //   //   columnGap: theme.spacing.xs,
+                            //   //   height: 128,
+                            //   // },
+                            // })}
+                            key={candidate.id}
+                          >
+                            <Box>
+                              {candidate.image_link ? (
+                                <Image
+                                  src={candidate.image_link}
+                                  alt="Candidate's image"
+                                  width={92}
+                                  height={92}
+                                  style={{
+                                    objectFit: "cover",
+                                  }}
+                                  priority
+                                />
+                              ) : (
+                                <IconUser
+                                  style={{ width: 92, height: 92, padding: 8 }}
+                                />
+                              )}
+                            </Box>
+
+                            <Text lineClamp={2} ta="center" hiddenFrom="sm">
+                              {candidate.first_name}{" "}
+                              {candidate.middle_name
+                                ? candidate.middle_name + " "
+                                : ""}
+                              {candidate.last_name} (
+                              {candidate.partylist.acronym})
+                            </Text>
+                            <Text lineClamp={2} ta="left" visibleFrom="sm">
+                              {candidate.first_name}{" "}
+                              {candidate.middle_name
+                                ? candidate.middle_name + " "
+                                : ""}
+                              {candidate.last_name} (
+                              {candidate.partylist.acronym})
+                            </Text>
+                          </UnstyledButton>
+                        ))
+                      )}
+                    </Group>
+                  </Stack>
+                ))}
+              </Stack>
+            </Stack>
+          </>
+        )}
+      </Container>
+    </>
+  );
 }
---
+++ apps/www/components/client/components/scroll-to-top.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Affix, Button, Transition, rem } from "@mantine/core";
+import { useWindowScroll } from "@mantine/hooks";
+import { IconArrowUp } from "@tabler/icons-react";
+
+export default function ScrollToTopButton() {
+  const [scroll, scrollTo] = useWindowScroll();
+  return (
+    <Affix position={{ bottom: rem(80), right: rem(20) }}>
+      <Transition transition="slide-up" mounted={scroll.y > 128}>
+        {(transitionStyles) => (
+          <Button
+            leftSection={<IconArrowUp size="1rem" />}
+            style={transitionStyles}
+            onClick={() => scrollTo({ y: 0 })}
+          >
+            Scroll to top
+          </Button>
+        )}
+      </Transition>
+    </Affix>
+  );
+}
