b5ac97bc58f1dc4be3967dea4d551f93973c7bb6
Brice Suazo <bricesuazo@gmail.com>
2023-08-15 09:46:07 +0800

    Bump to next@13.4.16. Make the /dashboard/[electionSlug]/* a nodejs runtime.

--- apps/www/app/(dashboard)/dashboard/[electionDashboardSlug]/layout.tsx
+++ apps/www/app/(dashboard)/dashboard/[electionDashboardSlug]/layout.tsx
@@ -1,5 +1,9 @@
 import { Box } from "@mantine/core";
 
+// TODO: Remove this
+//Error: The Edge Function "dashboard/[electionDashboardSlug]/*" size is 1.01 MB and your plan size limit is 1 MB. Learn More: https://vercel.link/edge-function-size
+export const runtime = "nodejs";
+
 export default function DashboardLayout(props: React.PropsWithChildren) {
   return <Box p="md">{props.children}</Box>;
 }
