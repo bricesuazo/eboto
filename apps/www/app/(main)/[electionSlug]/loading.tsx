d0219f9d1418e0c1668f19be1c254aa421ddfa30
Brice Suazo <bricesuazo@gmail.com>
2023-08-17 21:05:00 +0800

    Make edge. Add container to election loading skeleton.

--- apps/www/app/(main)/[electionSlug]/loading.tsx
+++ apps/www/app/(main)/[electionSlug]/loading.tsx
@@ -1,34 +1,36 @@
-import { Group, Skeleton, Stack } from "@mantine/core";
+import { Container, Group, Skeleton, Stack } from "@mantine/core";
 import React, { Fragment } from "react";
 
 export default function ElectionPageLoading() {
   return (
-    <Stack align="center" gap={12}>
-      <Stack gap={8} align="center">
-        <Group align="center" mb={4}>
-          <Skeleton w={92} h={92} />
-        </Group>
-        <Skeleton w={228} h={28} />
-        <Skeleton w={248} h={16} />
-        <Skeleton w={372} h={16} />
-        <Skeleton w={200} h={16} />
-        <Skeleton w={172} h={32} radius="lg" />
-      </Stack>
-
-      {[...(Array(8) as unknown as number[])].map((_, i) => (
-        <Stack key={i} gap={8} align="center" w="100%">
-          <Skeleton w={128} h={28} radius="lg" />
-
-          <Group align="center" w="100%" gap={12}>
-            {[...(Array(2) as unknown as number[])].map((_, i) => (
-              <Fragment key={i}>
-                <Skeleton w={200} h={192} radius="md" hiddenFrom="xs" />
-                <Skeleton w="100%" h={128} radius="md" visibleFrom="xs" />
-              </Fragment>
-            ))}
+    <Container py="xl">
+      <Stack align="center" gap={12}>
+        <Stack gap={8} align="center">
+          <Group align="center" mb={4}>
+            <Skeleton w={92} h={92} />
           </Group>
+          <Skeleton w={228} h={28} />
+          <Skeleton w={248} h={16} />
+          <Skeleton w={372} h={16} />
+          <Skeleton w={200} h={16} />
+          <Skeleton w={172} h={32} radius="lg" />
         </Stack>
-      ))}
-    </Stack>
+
+        {[...(Array(8) as unknown as number[])].map((_, i) => (
+          <Stack key={i} gap={8} align="center" w="100%">
+            <Skeleton w={128} h={28} radius="lg" />
+
+            <Group align="center" w="100%" gap={12}>
+              {[...(Array(2) as unknown as number[])].map((_, i) => (
+                <Fragment key={i}>
+                  <Skeleton w={200} h={192} radius="md" hiddenFrom="xs" />
+                  <Skeleton w="100%" h={128} radius="md" visibleFrom="xs" />
+                </Fragment>
+              ))}
+            </Group>
+          </Stack>
+        ))}
+      </Stack>
+    </Container>
   );
 }
