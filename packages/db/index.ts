68a8565a6f7bb5b5eeaba861c26555b1c60f30ae
Brice Suazo <bricesuazo@gmail.com>
2023-12-06 19:18:39 +0800

    Revert "chore: bump packages"
    This reverts commit c992f8a7eda1a0ad56e6eb8ad43616d41c88083f.

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,13 +1,10 @@
 import { connect } from "@planetscale/database";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
-import { migrate } from "drizzle-orm/planetscale-serverless/migrator";
 
 import * as schema from "./schema";
 
 const connection = connect({ url: process.env.DATABASE_URL });
 
 export const db = drizzle(connection, { schema });
-
-await migrate(db, { migrationsFolder: "generated" });
 
 export * from "drizzle-orm";
