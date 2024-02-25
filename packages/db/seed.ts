0a0c232751a9307589d79d34569bcc7ca821178a
Brice Suazo <bricesuazo@gmail.com>
2024-02-25 17:18:41 +0800

    feat: implement no. of voters

--- packages/db/seed.ts
+++ packages/db/seed.ts
@@ -8,6 +8,7 @@ interface LS_DATA_TYPE {
     variants: {
       id: number;
       name: string;
+      voters: number;
       price: number;
     }[];
   }[];
@@ -17,12 +18,12 @@ export const LS_DATA_DEV: LS_DATA_TYPE = {
     {
       id: 173086,
       name: "Free",
-      variants: [{ id: 222651, name: "Free", price: 0 }],
+      variants: [{ id: 222651, name: "Free", voters: 500, price: 0 }],
     },
     {
       id: 173087,
       name: "Plus",
-      variants: [{ id: 222652, name: "Plus", price: 199 }],
+      variants: [{ id: 222652, name: "Plus", voters: 0, price: 199 }],
     },
     {
       id: 173072,
@@ -31,26 +32,31 @@ export const LS_DATA_DEV: LS_DATA_TYPE = {
         {
           id: 222623,
           name: "1,500 Voters",
+          voters: 1500,
           price: 499,
         },
         {
           id: 222640,
           name: "2,500 Voters",
+          voters: 2500,
           price: 699,
         },
         {
           id: 222641,
           name: "5,000 Voters",
+          voters: 5000,
           price: 899,
         },
         {
           id: 222643,
           name: "7,500 Voters",
+          voters: 7500,
           price: 1099,
         },
         {
           id: 222644,
           name: "10,000 Voters",
+          voters: 10000,
           price: 1299,
         },
       ],
@@ -62,12 +68,12 @@ export const LS_DATA_PROD: LS_DATA_TYPE = {
     {
       id: 173116,
       name: "Free",
-      variants: [{ id: 222708, name: "Free", price: 0 }],
+      variants: [{ id: 222708, name: "Free", voters: 500, price: 0 }],
     },
     {
       id: 173117,
       name: "Plus",
-      variants: [{ id: 222709, name: "Plus", price: 199 }],
+      variants: [{ id: 222709, name: "Plus", voters: 0, price: 199 }],
     },
     {
       id: 173113,
@@ -76,26 +82,31 @@ export const LS_DATA_PROD: LS_DATA_TYPE = {
         {
           id: 222700,
           name: "1,500 Voters",
+          voters: 1500,
           price: 499,
         },
         {
           id: 222701,
           name: "2,500 Voters",
+          voters: 2500,
           price: 699,
         },
         {
           id: 222702,
           name: "5,000 Voters",
+          voters: 5000,
           price: 899,
         },
         {
           id: 222703,
           name: "7,500 Voters",
+          voters: 7500,
           price: 1099,
         },
         {
           id: 222704,
           name: "10,000 Voters",
+          voters: 10000,
           price: 1299,
         },
       ],
