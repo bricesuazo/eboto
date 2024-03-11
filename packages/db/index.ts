edca46113bcd297119dc8669783b388ac5d73d9c
Brice Suazo <bricesuazo@gmail.com>
2024-03-11 21:38:24 +0800

    fix: add pg and ws

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,7 +1,10 @@
-import { Pool } from "@neondatabase/serverless";
+import { neonConfig, Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
+import ws from "ws";
 
 import * as schema from "./schema";
+
+neonConfig.webSocketConstructor = ws as unknown as typeof WebSocket;
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
 
