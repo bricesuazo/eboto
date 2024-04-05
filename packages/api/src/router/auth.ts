43d0e34177b5b0ef2e4a51117eaf4ce08104836f
Brice Suazo <bricesuazo@gmail.com>
2024-04-05 20:15:46 +0800

    fix: getUser return

--- packages/api/src/router/auth.ts
+++ packages/api/src/router/auth.ts
@@ -2,9 +2,11 @@ import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const authRouter = createTRPCRouter({
   getUser: publicProcedure.query(({ ctx }) => {
+    if (!ctx.user) return null;
+
     let image_url: string | null = null;
 
-    if (ctx.user?.db.image_path) {
+    if (ctx.user.db.image_path) {
       const { data: image } = ctx.supabase.storage
         .from("users")
         .getPublicUrl(ctx.user.db.image_path);
@@ -12,7 +14,7 @@ export const authRouter = createTRPCRouter({
       image_url = image.publicUrl;
     }
 
-    return { ...ctx.user, db: { ...ctx.user?.db, image_url } };
+    return { ...ctx.user, db: { ...ctx.user.db, image_url } };
   }),
   getUserProtected: protectedProcedure.query(({ ctx }) => {
     let image_url: string | null = null;
