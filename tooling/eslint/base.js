ea6526b1c78954f21469f80513eb277a486dc47b
Brice Suazo <74240706+bricesuazo@users.noreply.github.com>
2025-03-03 13:53:20 +0800

    chore: update ESLint and TypeScript dependencies

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -1,5 +1,7 @@
 /// <reference types="./types.d.ts" />
 
+import * as path from "node:path";
+import { includeIgnoreFile } from "@eslint/compat";
 import eslint from "@eslint/js";
 import importPlugin from "eslint-plugin-import";
 import turboPlugin from "eslint-plugin-turbo";
@@ -8,35 +10,37 @@ import tseslint from "typescript-eslint";
 /**
  * All packages that leverage t3-env should use this rule
  */
-export const restrictEnvAccess = tseslint.config({
-  files: ["**/*.js", "**/*.ts", "**/*.tsx"],
-  rules: {
-    "no-restricted-properties": [
-      "error",
-      {
-        object: "process",
-        property: "env",
-        message:
-          "Use `import { env } from '~/env'` instead to ensure validated types.",
-      },
-    ],
-    "no-restricted-imports": [
-      "error",
-      {
-        name: "process",
-        importNames: ["env"],
-        message:
-          "Use `import { env } from '~/env'` instead to ensure validated types.",
-      },
-    ],
+export const restrictEnvAccess = tseslint.config(
+  { ignores: ["**/env.ts"] },
+  {
+    files: ["**/*.js", "**/*.ts", "**/*.tsx"],
+    rules: {
+      "no-restricted-properties": [
+        "error",
+        {
+          object: "process",
+          property: "env",
+          message:
+            "Use `import { env } from '~/env'` instead to ensure validated types.",
+        },
+      ],
+      "no-restricted-imports": [
+        "error",
+        {
+          name: "process",
+          importNames: ["env"],
+          message:
+            "Use `import { env } from '~/env'` instead to ensure validated types.",
+        },
+      ],
+    },
   },
-});
+);
 
 export default tseslint.config(
-  {
-    // Globally ignored files
-    ignores: ["**/*.config.*"],
-  },
+  // Ignore files not tracked by VCS and any config files
+  includeIgnoreFile(path.join(import.meta.dirname, "../../.gitignore")),
+  { ignores: ["**/*.config.*"] },
   {
     files: ["**/*.js", "**/*.ts", "**/*.tsx"],
     plugins: {
@@ -53,7 +57,7 @@ export default tseslint.config(
       ...turboPlugin.configs.recommended.rules,
       "@typescript-eslint/no-unused-vars": [
         "error",
-        { argsIgnorePattern: "_", varsIgnorePattern: "_" },
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
       ],
       "@typescript-eslint/consistent-type-imports": [
         "warn",
